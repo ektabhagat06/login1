a46b5459e452899f437557805434e8e77eed71f0
ektabhagat <ektabhagat06@outlook.com>
2021-07-20 10:33:34 +0530

    login1

---
+++ app/src/main/java/com/example/myapplication/login.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+
+
+    Button loginbutton,signupbtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        signupbtn=findViewById(R.id.signup);
+        loginbutton=findViewById(R.id.login);
+
+
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext().signup.class)
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
